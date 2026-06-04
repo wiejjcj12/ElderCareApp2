35fe55f90e10724845d15ae49212c6257024cfd9
wiejjcj12 <296255344@qq.com>
2026-06-04 21:39:19 +0800

    Add sw.js for PWA

---
+++ docs/sw.js
@@ -0,0 +1,26 @@
+// Service Worker - 缓存静态资源，支持离线使用
+const CACHE_NAME = 'elder-care-v1';
+const FILES = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
